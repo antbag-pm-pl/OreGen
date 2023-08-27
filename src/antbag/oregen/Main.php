c4c88693238c34ca41c664a66594adfd2637f02b
notant <84870445+antbag-dev@users.noreply.github.com>
2023-08-27 19:54:28 +0500

    Create Main.php

---
+++ src/antbag/oregen/Main.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace antbag\oregen;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\block\BlockFormEvent;
+use pocketmine\block\VanillaBlocks;
+
+class Main extends PluginBase {
+
+  public function onEnable(): void {
+    $this->saveDefaultConfig();
+    $this->getServer()->getPluginManager()->registerEvents($this, $this);
+  }
+
+  public function BlockUpdate(BlockFormEvent $event) {
+    $block = $event->getBlock();
+    $world = $block->getPosition()->getWorld();
+    $randomOre = mt_rand(1, 10);
+    
+    if ($randomOre <= 3) {
+      $oreType = mt_rand(1, 3);
+      $oreBlock = null;
+      switch ($oreType) {
+        case 1:
+          $oreBlock = VanillaBlocks::COAL_ORE();
+        break;
+        case 2:
+          $oreBlock = VanillaBlocks::IRON_ORE();
+        break;
+        case 3:
+          $oreBlock = VanillaBlocks::GOLD_ORE();
+        break;
+      }
+      if ($oreBlock !== null) {
+        $world->setBlock($block->getPosition(), $oreBlock);
+      }
+    }
+  }
+}
