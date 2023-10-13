ec0712576570bd5c23eed6cb623dcb797558f8b0
notant <84870445+antbag-dev@users.noreply.github.com>
2023-10-13 17:00:34 +0500

    Update Main.php

--- src/antbag/oregen/Main.php
+++ src/antbag/oregen/Main.php
@@ -15,26 +15,31 @@ class Main extends PluginBase {
   public function BlockUpdate(BlockFormEvent $event) {
     $block = $event->getBlock();
     $world = $block->getPosition()->getWorld();
-    $randomOre = mt_rand(1, 10);
-    
-    if ($randomOre <= 3) {
-      $oreType = mt_rand(1, 3);
-      $oreBlock = null;
-      switch ($oreType) {
-        case 1:
-          $oreBlock = VanillaBlocks::COAL_ORE();
-        break;
-        case 2:
-          $oreBlock = VanillaBlocks::IRON_ORE();
-        break;
-        case 3:
-          $oreBlock = VanillaBlocks::GOLD_ORE();
-        break;
-      }
-      if ($oreBlock !== null) {
+    $randomOre = mt_rand(1, 140);
+
+    if ($randomOre <= 50) {
+        $oreBlock = VanillaBlocks::COAL_ORE();
+    } elseif ($randomOre <= 90) {
+        $oreBlock = VanillaBlocks::IRON_ORE();
+    } elseif ($randomOre <= 120) {
+        $oreBlock = VanillaBlocks::GOLD_ORE();
+    } elseif ($randomOre <= 130) {
+        $oreBlock = VanillaBlocks::DIAMOND_ORE();
+    } elseif ($randomOre <= 135) {
+        $oreBlock = VanillaBlocks::EMERALD_ORE();
+    } elseif ($randomOre <= 137) {
+        $oreBlock = VanillaBlocks::LAPIS_LAZULI_ORE();
+    } elseif ($randomOre <= 138) {
+        $oreBlock = VanillaBlocks::REDSTONE_ORE();
+    } elseif ($randomOre <= 139) {
+        $oreBlock = VanillaBlocks::COPPER_ORE();
+    } else {
+        $oreBlock = VanillaBlocks::NETHER_QUARTZ_ORE();
+    }
+
+    if ($oreBlock !== null) {
         $event->cancel();
         $world->setBlock($block->getPosition(), $oreBlock);
-      }
     }
   }
 }
